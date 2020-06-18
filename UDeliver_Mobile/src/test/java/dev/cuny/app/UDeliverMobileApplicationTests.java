6534a2683e908cef2507ce4ed1035948a1b549ec
JRCode97 <35208196+JRCode97@users.noreply.github.com>
2020-06-18 15:43:39 -0400

    Update UDeliverMobileApplicationTests.java

--- UDeliver_Mobile/src/test/java/dev/cuny/app/UDeliverMobileApplicationTests.java
+++ UDeliver_Mobile/src/test/java/dev/cuny/app/UDeliverMobileApplicationTests.java
@@ -1,6 +1,7 @@
 package dev.cuny.app;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
